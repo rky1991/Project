e02c413ec8b0c5b0ad0a4d2db290c32f44e49018
TYSS <TYSS@TYSS-TR-002>
2019-01-02 16:34:53 +0530

    parallel execution

---
+++ Project/src/selenium/MultiBrowser.java
@@ -0,0 +1,79 @@
+package selenium;
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+ 
+import org.openqa.selenium.ie.InternetExplorerDriver;
+ 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+ 
+import org.testng.annotations.Test;
+ 
+public class MultiBrowser {
+ 
+ public WebDriver driver;
+ 
+  @Parameters("browser")
+ 
+  @BeforeMethod
+ 
+  // Passing Browser parameter from TestNG xml
+ 
+  public void beforeTest(String browser) {
+ 
+  // If the browser is Firefox, then do this
+ 
+  if(browser.equalsIgnoreCase("ie")) {
+	  System.setProperty("webdriver.ie.driver", "./driver/IEDriverServer.exe");
+ 
+	  driver = new InternetExplorerDriver();
+ 
+  // If browser is IE, then do this	  
+ 
+  }else if (browser.equalsIgnoreCase("chrome")) { 
+ 
+   // Here I am setting up the path for my IEDriver
+ 
+   System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
+   
+ 
+   driver = new ChromeDriver();
+   
+ 
+  } 
+ 
+  // Doesn't the browser type, lauch the Website
+ 
+  driver.get("http://www.store.demoqa.com"); 
+ 
+  }
+ 
+  // Once Before method is completed, Test method will start
+ 
+  @Test public void login() throws InterruptedException {
+ 
+ driver.findElement(By.xpath(".//*[@id='account']/a")).click();
+ 
+    driver.findElement(By.id("log")).sendKeys("testuser_1");
+ 
+    driver.findElement(By.id("pwd")).sendKeys("Test@123");
+ 
+    driver.findElement(By.id("login")).click();
+ 
+ }  
+ 
+  @AfterMethod
+  public void afterTest() {
+ 
+ driver.quit();
+ 
+ }
+ 
+}
