965ba51d55a131bda5369bc1687a63a53f72603d
rky1991 <43536498+rky1991@users.noreply.github.com>
2019-01-03 23:42:45 +0530

    old gecko

--- Project/src/TestNGExecution/FirefoxTest.java
+++ Project/src/TestNGExecution/FirefoxTest.java
@@ -1,19 +1,20 @@
 package TestNGExecution;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
 
 public class FirefoxTest {
 	static {
-		System.setProperty("webdriver.ie.driver","./driver/IEDriverServer.exe");
+		System.setProperty("webdriver.gecko.driver","./driver/geckodriver.exe");
 	}
 
 	@Test
 	public void fireFox() {
-		WebDriver driver = new InternetExplorerDriver();
+		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.google.com");
-		driver.quit();
+		//driver.quit();
 	}
 }
