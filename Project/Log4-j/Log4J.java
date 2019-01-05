e7dda2138ddf31c38e02048bef2e5cf032878ab2
rky1991 <rky1991@gmail.com>
2019-01-05 12:47:39 +0530

    log4j update

---
+++ Project/Log4-j/Log4J.java
@@ -0,0 +1,27 @@
+import org.apache.log4j.Logger;
+
+public class Log4J {
+
+	public static void main(String[] args) {
+		System.out.println("working start on log 4 j");
+		Logger logs = Logger.getLogger("Log4J");// argument of class name
+		logs.info("log4j is working fine");
+		logs.info("log4j is working fine");
+		logs.info("log4j is working fine");
+		logs.info("log4j is working fine");
+		logs.info("log4j is working fine");
+		logs.info("log4j is working fine");
+		logs.info("log4j is working fine");
+		
+		logs.info("log4j is working fine");logs.info("log4j is working fine");
+		logs.info("log4j is working fine");
+		logs.info("log4j is working fine");logs.info("log4j is working fine");logs.info("log4j is working fine");logs.info("log4j is working fine");
+		logs.info("log4j is working fine");
+		
+		logs.info("log4j is fine");
+		
+	
+
+	}
+
+}
---
+++ Project/src-ExtentReport/ExtentReport.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+public class ExtentReport {
+
+	public static void main(String[] args) throws IOException {
+		ExtentHtmlReporter reporter= new ExtentHtmlReporter("C:\\Users\\TYSS\\git\\Project\\Project\\ExtentReport\\firstRep.html");
+		ExtentReports extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		ExtentTest log=extent.createTest("main");
+		log.log(Status.INFO, "test is running");
+		log.log(Status.ERROR, "error due to some reason");
+		log.log(Status.WARNING, "worning");
+		
+		log.addScreenCaptureFromPath("C:\\Users\\TYSS\\Desktop\\rahul_selenium\\screenshots\\fs.png");
+		log.fail("failed due to some reason", MediaEntityBuilder.createScreenCaptureFromPath("C:\\Users\\TYSS\\Desktop\\rahul_selenium\\screenshots\\fs.png").build());
+		log.log(Status.FAIL, "failed");
+		extent.flush();
+		System.out.println("done");
+
+	}
+
+}
