2e28224a6549931b719e204b72926bbba2f2a80e
rky1991 <43536498+rky1991@users.noreply.github.com>
2019-01-06 20:39:04 +0530

    kk

--- Project/src-ExtentReport/ExtentReport.java
+++ Project/src-ExtentReport/ExtentReport.java
@@ -9,7 +9,7 @@ import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 public class ExtentReport {
 
 	public static void main(String[] args) throws IOException {
-		ExtentHtmlReporter reporter= new ExtentHtmlReporter("C:\\Users\\TYSS\\git\\Project\\Project\\ExtentReport\\firstRep.html");
+		ExtentHtmlReporter reporter= new ExtentHtmlReporter("C:\\Users\\Rahul\\git\\Project\\Project\\ExtentReport\\firstRep.html");
 		ExtentReports extent = new ExtentReports();
 		extent.attachReporter(reporter);
 		ExtentTest log=extent.createTest("main");
@@ -17,8 +17,8 @@ public class ExtentReport {
 		log.log(Status.ERROR, "error due to some reason");
 		log.log(Status.WARNING, "worning");
 		
-		log.addScreenCaptureFromPath("C:\\Users\\TYSS\\Desktop\\rahul_selenium\\screenshots\\fs.png");
-		log.fail("failed due to some reason", MediaEntityBuilder.createScreenCaptureFromPath("C:\\Users\\TYSS\\Desktop\\rahul_selenium\\screenshots\\fs.png").build());
+		log.addScreenCaptureFromPath("C:\\Users\\Rahul\\Desktop\\screenshots\\fs.png");
+		log.fail("failed due to some reason", MediaEntityBuilder.createScreenCaptureFromPath("C:\\Users\\Rahul\\Desktop\\screenshots\\fs.png").build());
 		log.log(Status.FAIL, "failed");
 		extent.flush();
 		System.out.println("done");
